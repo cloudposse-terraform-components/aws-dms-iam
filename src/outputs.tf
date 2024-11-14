126139e077a10dba16d8e81ec17c229337476e9a
Igor Rodionov <goruha@gmail.com>
2024-11-14 20:20:08 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,14 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "dms_redshift_s3_role_arn" {
+  value       = module.dms_iam.dms_redshift_s3_role_arn
+  description = "DMS Redshift S3 role ARN"
+}
+
+output "dms_cloudwatch_logs_role_arn" {
+  value       = module.dms_iam.dms_cloudwatch_logs_role_arn
+  description = "DMS CloudWatch Logs role ARN"
+}
+
+output "dms_vpc_management_role_arn" {
+  value       = module.dms_iam.dms_vpc_management_role_arn
+  description = "DMS VPC management role ARN"
 }
